de0b457a40eef6d9117e075c4722857f4ef4a519
Brioflator <balescu.alexandru2002@gmail.com>
2024-02-08 22:14:16 +0200

    modified LogIn and Register pages

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -6,6 +6,8 @@ import {
   Heading,
   Input,
   Text,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
@@ -19,6 +21,9 @@ const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
 
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
@@ -45,41 +50,57 @@ const RegisterPage = () => {
       <Center
         flexDirection="column"
         p="20"
-        bg="blue.100"
+        bg="teal.50"
         boxShadow="2"
         borderRadius="lg"
       >
-        <Heading>Register</Heading>
+        <Heading
+          bgGradient="linear(to-l,#00E9F5 ,#61E294 )"
+          bgClip="text"
+          fontSize="5xl"
+          fontWeight="extrabold"
+          pb={2}
+        >
+          Regsiter
+        </Heading>
         <FormControl>
-          <FormLabel>Email</FormLabel>
+          <FormLabel>E-mail</FormLabel>
           <Input
             value={email}
+            placeholder="Enter E-mail"
             onChange={(event) => {
               setEmail(event.target.value);
               setError("");
             }}
-            bg="white"
             type="email"
           />
         </FormControl>
 
         <FormControl mt="5">
           <FormLabel>Password</FormLabel>
-          <Input
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-              setError("");
-            }}
-            bg="white"
-            type="password"
-          />
+          <InputGroup size="md">
+            <Input
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              placeholder="Enter password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+                setError("");
+              }}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick} bg="#61E294">
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
         </FormControl>
 
         {error && <Text color="red">{error}</Text>}
 
         <Text
-          color="blue"
+          color="green"
           onClick={() => {
             navigate("/login");
           }}
@@ -87,7 +108,7 @@ const RegisterPage = () => {
           Do you have an account?
         </Text>
 
-        <Button mt="5" onClick={onRegister}>
+        <Button mt="5" onClick={onRegister} bg="#61E294">
           Register
         </Button>
       </Center>
